9182c834fc72874ad00d1018093de39172b28a82
ritwit <rithwiktomtcr@gmail.com>
2019-10-29 13:52:43 -0400

    test script fjor mpi_pygenarris

--- test.py
+++ test.py
@@ -6,18 +6,23 @@ Created on Thu May 30 16:36:06 2019
 @author: ritwit
 """
 
-from pygenarris import *
+from pygenarris_mpi import *
 import numpy as np
+from mpi4py import MPI
 
+cutoff = np.load('cutoff_gly4mpc.npy');
+cutoff=np.array(cutoff, dtype='float32');
+print(cutoff);
 
-lattice = np.eye(3)
-xc = np.array([1, 1.3, 1.6]);
-yc = np.array([1, 1.3, 1.6]);
-zc = np.array([1, 1.3, 1.6]);
-atoms = "C C C "
-xtal = crystal();
-vdw_matrix = np.array([[0, 1, 1],[1 , 0, 1],[ 1, 1, 0]], dtype = 'float32')
+seedstate = 39944;
+filename='geometry.out';
+num_structures = 10;
+Z = 4;
+volume_mean = 380;
+volume_std = 18;
+tol = 0.0001;
+max_attempts = 2000000000;
+comm = MPI.COMM_WORLD;
 
+mpi_generate_molecular_crystals_with_vdw_cutoff_matrix(cutoff, num_structures, Z, volume_mean, volume_std, tol, max_attempts, comm);
 
-create_crystal_from_array(xtal, lattice, xc, yc, zc, atoms,3, 3, 1 )
-check_structure_with_vdw_matrix(xtal,  vdw_matrix)
