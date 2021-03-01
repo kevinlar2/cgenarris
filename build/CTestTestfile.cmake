358581a85548ae35d4a677936187ad4ada59f289
wwwaj <wenwen@andrew.cmu.edu>
2021-03-01 13:05:02 -0500

    all merge is done and build deleted

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wwen/Merged_Genarris/Fork_Version/cgenarris
-# Build directory: /home/wwen/Merged_Genarris/Fork_Version/cgenarris/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(cgenarris_test "/home/wwen/Merged_Genarris/Fork_Version/cgenarris/build/cgenarris_test")
-set_tests_properties(cgenarris_test PROPERTIES  WORKING_DIRECTORY "/home/wwen/Merged_Genarris/Fork_Version/cgenarris/test/regression_test1/")
