701015e8a9940e9698c48a5402a2dea4d9aa5de1
ritwit <rithwikiisc@gmail.com>
2021-04-26 15:12:55 -0400

    Add main cxtal generation loop

--- src/pygenarris_mpi_utils.c
+++ src/pygenarris_mpi_utils.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 #include "molecule_utils.h"
+#include "pygenarris_mpi_utils.h"
 #include "randomgen.h"
 
 void print_time(void)
@@ -81,3 +82,88 @@ void get_n_atoms_in_mol(int *n_atoms_in_mol, molecule *mol, int n_mol_types)
     }
 }
 
+
+void print_allowed_spg(int *allowed_spg, int num_spg)
+{
+    printf("Detecting compatible spacegroups using only general Wyckoff positions:\n");
+    printf("Number of allowed spg: %d\n", num_spg);
+    for(int spg_id = 0; spg_id < num_spg; spg_id++)
+    {
+        int spg = allowed_spg[spg_id];
+        printf("Spacegroup %d is compatible.\n", spg);
+    }
+    printf("\n");
+}
+
+/*
+main structure generation loop
+*/
+int try_crystal_generation(cocrystal cxtal,
+                           Settings set,
+                           float *volume,
+                           float *vdw_matrix,
+                           long attempts,
+                           long batch_size,
+                           int spg)
+{
+    cxtal.spg = spg;
+    // Loop over the batch. bat = batch attempt
+    for(long bat = 0; bat < batch_size; bat++)
+    {
+        // Attempt one generation
+        int result = generate_cocrystal(cxtal, set, *volume, vdw_matrix);
+
+        // Alignment failures
+        if(!result)
+            continue;
+
+        // reset volume
+        if( (attempts + bat) % set.vol_attempts == 0)
+        {
+            *volume = draw_volume(set.vol_mean, set.vol_std);
+            fflush(stdout);
+        }
+
+        // Structure check
+        int verdict = cxtal_check_structure(cxtal, vdw_matrix);
+        if(verdict)
+            break;
+    }
+
+}
+
+
+/*
+Checks if structure generation can be stopped.
+Check if enough structures were generated. OR
+ran out of attempts
+*/
+int check_stop_condition()
+{
+    return 1;
+}
+
+
+/*
+Slave processes send structures to master rank
+*/
+int send_structures()
+{
+    return 1;
+}
+
+/*
+Master rank write to output file
+*/
+int write_structures()
+{
+    return 1;
+}
+
+/*
+Print after Genarris exits
+*/
+void print_exit()
+{
+    printf("Genarris has completed generation.\n");
+}
--- src/pygenarris_mpi_utils.h
+++ src/pygenarris_mpi_utils.h
@@ -1,6 +1,9 @@
 #ifndef _PYGENARRIS_MPI_UTILS_H_
 #define _PYGENARRIS_MPI_UTILS_H_
 
+#include "input_settings.h"
+#include "cocrystal.h"
+
 void print_time(void);
 FILE* open_output_file(int my_rank);
 void init_random_seed(unsigned int *seed, unsigned int *seed2,
@@ -8,6 +11,14 @@ void init_random_seed(unsigned int *seed, unsigned int *seed2,
 void recenter_molecules(molecule* mol, int mol_types);
 float draw_volume(float volume_mean, float volume_std);
 void get_n_atoms_in_mol(int *n_atoms_in_mol, molecule *mol, int n_mol_types);
+void print_allowed_spg(int *allowed_spg, int num_spg);
+int try_crystal_generation(cocrystal cxtal,
+                           Settings set,
+                           float *volume,
+                           float *vdw_matrix,
+                           long attempts,
+                           long batch_size,
+                           int spg);
 
 
 #endif //  pygenarris_mpi_utils.h
