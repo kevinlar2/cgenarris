24d646b2070c0fce194a8802a63f97bfc949bbde
ritwit <rithwikiisc@gmail.com>
2021-05-16 10:43:21 -0400

    Add cxtal structure check

--- src/cocrystal_utils.c
+++ src/cocrystal_utils.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "cocrystal_utils.h"
+#include "check_structure.h"
+
 
 void cxtal_init(cocrystal *cxtal, int *stoic, int *n_atoms_in_mol, int n_mol_types, int Z)
 {
@@ -27,6 +30,7 @@ void cxtal_init(cocrystal *cxtal, int *stoic, int *n_atoms_in_mol, int n_mol_typ
     cxtal->wyckoff_position  = (int *) malloc(tbytes);
     cxtal->mol_index = (int *) malloc(mbytes);
     cxtal->mol_types = (int *) malloc(mbytes);
+    cxtal->n_mols = n_mol_asym * Z;
     cxtal_allocate(cxtal, n_atoms);
 
     // store details to cxtal
@@ -35,7 +39,6 @@ void cxtal_init(cocrystal *cxtal, int *stoic, int *n_atoms_in_mol, int n_mol_typ
     memcpy(cxtal->n_atoms_in_mol, n_atoms_in_mol, tbytes);
     cxtal->n_mol_types = n_mol_types;
     cxtal->n_atoms = n_atoms;
-    cxtal->n_mols = n_mol_asym * Z;
     cxtal->spg = 0;
     cxtal->Zp = 0;
 
@@ -57,29 +60,33 @@ void cxtal_init(cocrystal *cxtal, int *stoic, int *n_atoms_in_mol, int n_mol_typ
     }
 }
 
+
 void cxtal_allocate(cocrystal *cxtal, int total_atoms)
 {
-    cxtal->Xcord = (float *) malloc(total_atoms * sizeof(float));
-    cxtal->Ycord = (float *) malloc(total_atoms * sizeof(float));
-    cxtal->Zcord = (float *) malloc(total_atoms * sizeof(float));
-    cxtal->atoms = (char *)  malloc(total_atoms * sizeof(char) * 2);
+    int fbytes = total_atoms * sizeof(float);
+    cxtal->Xcord = malloc(fbytes);
+    cxtal->Ycord = malloc(fbytes);
+    cxtal->Zcord = malloc(fbytes);
+    cxtal->com   = malloc(3 * cxtal->n_mols * sizeof(float));
+    cxtal->atoms = malloc(total_atoms * sizeof(char) *2);
 }
+
 
 void cxtal_print(cocrystal *cxtal, FILE* out, int fractional)
 {
-    printf("#Number of atoms = %d\n", cxtal->n_atoms);
-    printf("#Number of molecules = %d\n", cxtal->n_mols);
-    printf("#Number of molecule types = %d\n", cxtal->n_mol_types);
-    printf("#Z = %d\n", cxtal->Z);
+    fprintf(out, "#Number of atoms = %d\n", cxtal->n_atoms);
+    fprintf(out, "#Number of molecules = %d\n", cxtal->n_mols);
+    fprintf(out, "#Number of molecule types = %d\n", cxtal->n_mol_types);
+    fprintf(out, "#Z = %d\n", cxtal->Z);
 
     // Print mol index
-    printf("#mol_index = ");
+    fprintf(out, "#mol_index = ");
     for(int i = 0; i < cxtal->n_mols; i++)
-        printf("%d ", cxtal->mol_index[i]);
-    printf("\n#mol_types = ");
+        fprintf(out, "%d ", cxtal->mol_index[i]);
+    fprintf(out, "\n#mol_types = ");
     for(int i = 0; i < cxtal->n_mols; i++)
-        printf("%d ", cxtal->mol_types[i]);
-    printf("\n");
+        fprintf(out, "%d ", cxtal->mol_types[i]);
+    fprintf(out, "\n");
 
     for(int ltt = 0; ltt < 3; ltt++)
     {
@@ -88,7 +95,6 @@ void cxtal_print(cocrystal *cxtal, FILE* out, int fractional)
                 cxtal->lattice_vectors[ltt][1],
                 cxtal->lattice_vectors[ltt][2]);
     }
-
 
     for(int at = 0; at < cxtal->n_atoms; at++)
     {
@@ -104,7 +110,74 @@ void cxtal_print(cocrystal *cxtal, FILE* out, int fractional)
     }
 }
 
+
+void cxtal_compute_COM(cocrystal *cxtal)
+{
+    for(int mol = 0; mol < cxtal->n_mols; mol++)
+    {
+        int n_atom_mol = cxtal->n_atoms_in_mol[cxtal->mol_types[mol]];
+        float com[3] = {0.0, 0.0, 0.0};
+
+        int start_id = cxtal->mol_index[mol];
+        int end_id = cxtal->mol_index[mol] + n_atom_mol;
+        for(int mat = start_id; mat < end_id; mat++)
+        {
+            com[0] += cxtal->Xcord[mat];
+            com[1] += cxtal->Ycord[mat];
+            com[2] += cxtal->Zcord[mat];
+        }
+        com[0] /= n_atom_mol;
+        com[1] /= n_atom_mol;
+        com[2] /= n_atom_mol;
+
+        memcpy(cxtal->com[mol], com, sizeof(float)*3);
+    }
+}
+
+
 void cxtal_bring_molecules_first_cell(cocrystal *cxtal)
 {
+    float inv_lat_vec[3][3];
+    float trans_lat_vec[3][3];
+    mat3b3_transpose(trans_lat_vec, cxtal->lattice_vectors);
+    inverse_mat3b3(inv_lat_vec, trans_lat_vec);
+
+    cxtal_compute_COM(cxtal);
+
+    for(int mol = 0; mol < cxtal->n_mols; mol++)
+    {
+        // Compute COM shift - integer part of com in fractional cords
+        float com_frac[3];
+        memcpy(com_frac, cxtal->com[mol], 3*sizeof(float));
+        vector3_mat3b3_multiply(inv_lat_vec, com_frac);
+        vector3_int(com_frac);
+        vector3_mat3b3_multiply(trans_lat_vec, com_frac);
+
+        int n_atom_mol = cxtal->n_atoms_in_mol[cxtal->mol_types[mol]];
+        int start_id = cxtal->mol_index[mol];
+        int end_id = cxtal->mol_index[mol] + n_atom_mol;
+        for(int mat = start_id; mat < end_id; mat++)
+        {
+            cxtal->Xcord[mat] -= com_frac[0];
+            cxtal->Ycord[mat] -= com_frac[1];
+            cxtal->Zcord[mat] -= com_frac[2];
+        }
+    }
+
+    cxtal_compute_COM(cxtal);
+}
+
+
+int cxtal_check_structure(cocrystal *cxtal, Settings *set)
+{
+    crystal xtal;
+    memcpy(xtal.lattice_vectors, cxtal->lattice_vectors, 9*sizeof(float));
+    xtal.Xcord = cxtal->Xcord;
+    xtal.Ycord = cxtal->Ycord;
+    xtal.Zcord = cxtal->Zcord;
+    xtal.atoms = cxtal->atoms;
+
+    return structure_checker(&xtal, set->vdw_matrix,
+        cxtal->n_atoms, cxtal->mol_index, cxtal->n_mols);
 
 }
